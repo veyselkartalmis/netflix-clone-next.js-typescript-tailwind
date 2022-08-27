be2d33329bcfc7666164684416829aa42d34f682
Veysel <veyselkartalms@gmail.com>
2022-08-27 19:52:27 +0300

    header added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+
 function Header() {
     return (
         <header>
@@ -6,6 +9,7 @@ function Header() {
                     src="https://rb.gy/ulxxee"
                     width={100}
                     height={100}
+                    alt="logo"
                     className="cursor-pointer object-contain"
                 />
                 <ul className="hidden space-x-4 md:flex">
@@ -16,9 +20,20 @@ function Header() {
                     <li className="headerLink">My List</li>
                 </ul>
             </div>
-            <div></div>
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <SearchIcon className="hidden h-6 w-6 sm:inline" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="h-6 w-6" />
+                <Link href="/account">
+                    <img
+                        src="https://rb.gy/g1pwyx"
+                        alt="account"
+                        className="cursor-pointer rounded"
+                    />
+                </Link>
+            </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
