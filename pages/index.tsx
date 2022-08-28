6c4589174d2b0b2638b2eaaee4565ebe2400e0cd
Veysel <veyselkartalms@gmail.com>
2022-08-28 20:05:13 +0300

    custom bg-gradient added -thumbnail

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home = ({
   romanceMovies,
   documantaries, }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -34,7 +34,7 @@ const Home = ({
 
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-20">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Movies" movies={actionMovies} />
